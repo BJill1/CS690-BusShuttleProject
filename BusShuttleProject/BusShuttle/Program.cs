3dcd858af6bfe744f6c9a0d5afa5c32e7313d972
BJill1 <157993588+BJill1@users.noreply.github.com>
2025-04-13 18:45:48 +0000

    Initial draft

--- BusShuttleProject/BusShuttle/Program.cs
+++ BusShuttleProject/BusShuttle/Program.cs
@@ -1,9 +1,26 @@
 ﻿namespace BusShuttle;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Please select mode (driver or manager): ");
+        string mode = Console.ReadLine();
+        if(mode == "driver"){
+            string command;
+            do
+            {
+                Console.Write("Enter stop name: ");
+                string StopName = Console.ReadLine();
+
+                Console.Write("Enter the number of boarded passengers: ");
+                int boarded = int.Parse(Console.ReadLine());
+                File.AppendAllText("passenger_data.txt", StopName + ": " + boarded + Environment.NewLine);
+
+                Console.Write("Please enter command (end or continue): ");
+                command = Console.ReadLine();
+            } while(command != "end");
+        }
     }
 }
