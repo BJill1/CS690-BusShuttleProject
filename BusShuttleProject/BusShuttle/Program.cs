951bd8858a61a6e75a22ee0a2b956999d47643cc
BJill1 <157993588+BJill1@users.noreply.github.com>
2025-04-14 06:11:10 +0000

    Some refactoring

--- BusShuttleProject/BusShuttle/Program.cs
+++ BusShuttleProject/BusShuttle/Program.cs
@@ -5,22 +5,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Please select mode (driver or manager): ");
-        string mode = Console.ReadLine();
+        FileSaver filesaver = new FileSaver("passenger_data.txt");
+        string mode = AskForInput("Please select mode (driver or manager): ");
         if(mode == "driver"){
             string command;
             do
             {
-                Console.Write("Enter stop name: ");
-                string StopName = Console.ReadLine();
+                string StopName = AskForInput("Enter stop name: ");
 
-                Console.Write("Enter the number of boarded passengers: ");
-                int boarded = int.Parse(Console.ReadLine());
-                File.AppendAllText("passenger_data.txt", StopName + ": " + boarded + Environment.NewLine);
-
-                Console.Write("Please enter command (end or continue): ");
-                command = Console.ReadLine();
+                int boarded = int.Parse(AskForInput("Enter the number of boarded passengers: "));
+                filesaver.AppendLine(StopName + ": " + boarded);
+                
+                command = AskForInput("Please enter command (end or continue): ");
             } while(command != "end");
         }
     }
+    public static string AskForInput(string message)
+    {
+        Console.Write(message);
+        return Console.ReadLine();
+    }
+}
+public class FileSaver
+{
+    string FileName;
+    public FileSaver(string FileName)
+    {
+        this.FileName = FileName;
+        File.Create(this.FileName).Close();
+    }
+    public void AppendLine(string line)
+    {
+        File.AppendAllText(this.FileName, line+Environment.NewLine);
+    }
+    
+
 }
