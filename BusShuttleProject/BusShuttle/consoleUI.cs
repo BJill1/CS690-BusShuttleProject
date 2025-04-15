7472f3f6ff932226554c7944d516435518df5366
BJill1 <157993588+BJill1@users.noreply.github.com>
2025-04-15 10:46:44 +0000

    With more refactoring

--- BusShuttleProject/BusShuttle/consoleUI.cs
+++ BusShuttleProject/BusShuttle/consoleUI.cs
@@ -4,9 +4,30 @@ using Spectre.Console;
 public class consoleUI
 {
     FileSaver filesaver;
+    List<Loop> loops;
+    List<Stop> stops;
+
     public consoleUI()
     {
         filesaver = new FileSaver("passenger_data.txt");
+        loops = new List<Loop>();
+        loops.Add(new Loop("Red"));
+        loops.Add(new Loop("Green"));
+        loops.Add(new Loop("Blue"));
+
+        stops = new List<Stop>();
+        stops.Add(new Stop("Music"));
+        stops.Add(new Stop("Tower"));
+        stops.Add(new Stop("Oakwood"));
+        stops.Add(new Stop("Anthony"));
+        stops.Add(new Stop("Letterman"));
+
+        loops[0].Stops.Add(stops[0]);
+        loops[0].Stops.Add(stops[1]);
+        loops[0].Stops.Add(stops[2]);
+        loops[0].Stops.Add(stops[3]);
+        loops[0].Stops.Add(stops[4]);
+
     }
     public void show()
     {
@@ -19,12 +40,21 @@ public class consoleUI
         }));
         if(mode == "driver"){
             string command;
+            Loop SelectedLoop = AnsiConsole.Prompt(
+                    new SelectionPrompt<Loop>()
+                    .Title("Select a loop:")
+                    .PageSize(10)
+                    .AddChoices(loops));
             do
             {
-                string StopName = AskForInput("Enter stop name: ");
+                Stop SelectedStop = AnsiConsole.Prompt(
+                    new SelectionPrompt<Stop>()
+                    .Title("Select a stop:")
+                    .PageSize(10)
+                    .AddChoices(SelectedLoop.Stops));
 
                 int boarded = int.Parse(AskForInput("Enter the number of boarded passengers: "));
-                filesaver.AppendLine(StopName + ": " + boarded);
+                filesaver.AppendLine(SelectedStop.Name + ": " + boarded);
                 
                     command = AnsiConsole.Prompt(
                     new SelectionPrompt<string>()
